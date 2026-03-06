3f34e7ffd648ac1469352246bc6001dbe8967473
lorenzot08 <lorenzo.torcolacci.stud@ispascalcomandini.it>
2026-03-06 09:12:53 +0100

    Creato progetto

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Programmazione_Asincrona_Lettere
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
